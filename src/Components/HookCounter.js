40915732e532b5410b648d01286afff0f570fbf5
prajwalzingare <74585612+prajwalzingare@users.noreply.github.com>
2022-03-08 13:55:39 +0530

    tut2

--- src/Components/HookCounter.js
+++ src/Components/HookCounter.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react"; //we import use state by writing usestate in braces.
 
 export default function HookCounter() {
-  //hooks are function so we call them.
-  const [count, setCount] = useState(0);
+  //hooks are function so we call them.and give initial value as argument
+  const [count, setCount] = useState(0); //this syntax is called as array destructuring.
   return (
     <div>
-      <h1>counter{}</h1>
-      <button onClick={increse}>Click to increse</button>
+      <h1>counter {count}</h1>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Click to increse
+      </button>
     </div>
   );
 }
+//Rules for react Hooks-1)only call hooks at the top level.
+// Dont call hooks inside loops condition or nested function.
+//2)only call hooks from react function.
