9e5278cfaf3e88dd21984b97f5412d5769dd62a4
prajwalzingare <74585612+prajwalzingare@users.noreply.github.com>
2022-04-03 10:24:09 +0530

    data fetching

---
+++ src/Components/DataFetching.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+function DataFetching() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => {
+        console.log(res);
+        setPosts(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []); //without dependancy array loop will be infinite so you should apply dependancy.kisipe depend nahi rahega
+  return (
+    <div>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default DataFetching;
