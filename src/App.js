b4febc9da348cdf3fcc75f314f4712f8a61c25fa
prajwalzingare <74585612+prajwalzingare@users.noreply.github.com>
2022-04-03 10:22:58 +0530

    file changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import "./App.css";
 import ClassCounterOne from "./Components/ClassCounterOne";
+import ClassMouse from "./Components/ClassMouse";
+import DataFetching from "./Components/DataFetching";
 import HookCounterFour from "./Components/HookCounterFour";
 import HookCounterOne from "./Components/HookCounterOne";
 // import ClassCounter from "./Components/ClassCounter";
 import HookCounterThree from "./Components/HookCounterThree";
+import HookMouse from "./Components/HookMouse";
+import IntervalClassCounter from "./Components/IntervalClassCounter";
+import IntervalHookCounter from "./Components/IntervalHookCounter";
+import MouseContainer from "./Components/MouseContainer";
 // import HookCounter from "./Components/HookCounter";
 // import HookCounterTwo from "./Components/HookCounterTwo";
 import Tut1 from "./Components/Tut1";
+import UseEfect1 from "./Components/UseEfect1";
+import UseEffect2 from "./Components/UseEffect2";
 import { UseStateArray } from "./Components/UseStateArray";
 
 function App() {
   return (
     <div className="App">
+      <UseEffect2 />
+      {/* <UseEfect1 /> */}
+      {/* <DataFetching /> */}
+      {/* <IntervalHookCounter /> */}
+      {/* <IntervalClassCounter /> */}
+      {/* <MouseContainer /> */}
+      {/* <HookMouse /> */}
+      {/* <ClassMouse /> */}
       {/* <ClassCounterOne /> */}
-      <HookCounterOne />
+      {/* <HookCounterOne /> */}
       {/* <UseStateArray /> */}
       {/* <HookCounterFour /> */}
       {/* <HookCounterThree /> */}
