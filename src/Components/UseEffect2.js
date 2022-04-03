ed34d4040b9c243df49e8154f9cec9137e04002f
prajwalzingare <74585612+prajwalzingare@users.noreply.github.com>
2022-04-03 10:26:39 +0530

    Thapa technical

---
+++ src/Components/UseEffect2.js
@@ -0,0 +1,13 @@
+//This tut is from Thapa technical-https://www.youtube.com/watch?v=5gCtW7RCtQA&t=9s&ab_channel=ThapaTechnical
+import React, { useState, useEffect } from "react";
+
+function UseEffect2() {
+  return (
+    <div>
+      <p>The actual size of the window is</p>
+      <h1>{1920}</h1>
+    </div>
+  );
+}
+
+export default UseEffect2;
